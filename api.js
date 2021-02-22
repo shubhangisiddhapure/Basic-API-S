34028c6f6e8c21dbf981d9e689422923d37c852e
shubhangisiddhapure <79439864+shubhangisiddhapure@users.noreply.github.com>
2021-02-22 16:55:16 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,35 @@
+var express = require('express')
+const  app = express()
+const user=require('./user.json')
+const fs = require('fs')
+const port =3001
+
+app.use(express.json())
+
+app.get('/get',(req,res)=>{
+    console.log(user)
+    res.send(user)
+})
+
+app.post('/post',(req,res)=>{
+    var add={
+        "name":req.body.name,
+        "age":req.body.age,
+        "lastname":req.body.lastname
+    }
+    user.push(add)
+    fs.writeFile('user.json',JSON.stringify(user),err=>{
+        if(err){
+            console.log(err)
+            res.send(err)
+        }
+        else{
+            console.log('posted..')
+            res.send("data post")
+        }
+    })
+})
+
+app.listen(port,()=>{
+    console.log('server started on port 3000')
+})
